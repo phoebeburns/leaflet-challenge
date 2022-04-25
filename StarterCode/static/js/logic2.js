c151ccc53af94aa0918313fc2e8267161d45bdae
phoebeburns <37161658+phoebeburns@users.noreply.github.com>
2022-04-25 19:59:06 -0400

    Update logic2.js

--- StarterCode/static/js/logic2.js
+++ StarterCode/static/js/logic2.js
@@ -1,6 +1,6 @@
 
 // Store our API endpoint as queryUrl.
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
 
 d3.json(queryUrl).then(function (data) {
     // Once we get a response, send the data.features object to the createFeatures function.
@@ -8,16 +8,16 @@ d3.json(queryUrl).then(function (data) {
 });
 
 // Create the base layers.
-var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright"<OpenStreetMap</a< contributors'
 })
 
-var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright"<OpenStreetMap</a< contributors, <a href="http://viewfinderpanoramas.org"<SRTM</a< | Map style: &copy; <a href="https://opentopomap.org"<OpenTopoMap</a< (<a href="https://creativecommons.org/licenses/by-sa/3.0/"<CC-BY-SA</a<)'
 });
 
 // Create a baseMaps object.
-var baseMaps = {
+let baseMaps = {
     "Street Map": street,
     "Topographic Map": topo
 };
@@ -26,7 +26,7 @@ var baseMaps = {
 function getColor(d){
     switch (true) {
         case d < 10:
-            mcolor = "red";
+            mcolor = "red"; 
             break;
         case d < 15:
             mcolor = "orange";
@@ -44,13 +44,34 @@ function getColor(d){
     return mcolor
 }
 
-var myMap = L.map("map", {
+let map = L.map("map", {
     center: [
         -43.382, 10.7447, 10
     ],
     zoom: 1,
     layers: [street]
 })
+
+let legend = L.control({ position: "bottomright" });
+
+legend.onAdd = function (map) {
+
+    let div = L.DomUtil.create("div", "info legend"),
+        labels = ['<strong>Categories</strong>'],
+        categories = ["0 - 9", "10 - 24", "25 - 74", "75 - 150", ">= 150"];
+        categories2 = ["5", "15","50", "100","199"]
+
+    div.innerHTML = '';
+    for (var i = 0; i < categories2.length; i++) {
+        labels.push(
+            '<i class="circle" style="background:' + getColor(categories2[i]) + '"></i> ' +
+            (categories[i] ? categories[i] : '+'));
+    }
+    div.innerHTML = labels.join('<br>');
+    console.log(labels)
+};
+
+legend.addTo(map);
 
 
 function createMap(earthquakeData) {
@@ -73,7 +94,7 @@ function createMap(earthquakeData) {
                 layer: earthquakes
             });
 
-            marker.addTo(myMap);
+            marker.addTo(map);
             marker.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>
                                   <hr>Maximum reported intensity: ${feature.properties.cdi}</p>
                                   <hr>Depth: ${feature.geometry.coordinates[2]}</p>`
